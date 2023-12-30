13a542705dd697291a42fd80e5528b4cd641362d
Anubhab Mandal <anubhab.saie@gmail.com>
2023-12-30 20:22:21 +0530

    added beam search

--- inference_nlg.py
+++ inference_nlg.py
@@ -71,6 +71,7 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--test_data", type=str)
     parser.add_argument("--model_dir", type=str)
+    parser.add_argument("--inference_method", type=str, choices=["top-p", "beam"], default="beam")
     """
     parser.add_argument('--num_epochs', type=int, default=3)
     parser.add_argument('--lr', type=int, default=1e-3)
@@ -156,6 +157,62 @@ def main(args):
             # Decode the output sequence
             output_text = tokenizer.decode(decoder_input_ids[0], skip_special_tokens=True)
             return output_text, output_probs
+        
+    from torch.nn.functional import log_softmax
+
+    def generate_completion_with_beam_search(
+        model, tokenizer, text, max_length=50, beam_size=3
+    ):
+        model.eval()
+        with torch.no_grad():
+            input_ids = tokenizer.encode(text, return_tensors="pt").to(device)
+            
+            pad_token_id = tokenizer.pad_token_id
+            decoder_input_ids = torch.full((input_ids.shape[0], 1), pad_token_id, dtype=torch.long, device=input_ids.device)
+            
+            # Prepare output probabilities list and beam list
+            output_probs = []
+            beams = [(decoder_input_ids, 1.0)]
+            
+            for _ in range(max_length):
+                new_beams = []
+                for beam_input_ids, beam_prob in beams:
+                    # Get logits for the next token
+                    outputs = model(input_ids=input_ids, decoder_input_ids=beam_input_ids, return_dict=True)
+                    next_token_logits = outputs.logits[:, -1, :]
+
+                    # Apply log_softmax to get log probabilities
+                    log_probs = log_softmax(next_token_logits, dim=-1)
+
+                    # Get top K tokens and their corresponding log probabilities
+                    topk_probs, topk_indices = torch.topk(log_probs, beam_size, dim=-1)
+
+                    for i in range(beam_size):
+                        next_token = topk_indices[0, i].unsqueeze(0)
+                        next_token_prob = topk_probs[0, i].item()
+
+                        new_beam_input_ids = torch.cat([beam_input_ids, next_token], dim=1)
+                        new_beam_prob = beam_prob * next_token_prob
+
+                        new_beams.append((new_beam_input_ids, new_beam_prob))
+
+                # Select top K beams based on their probabilities
+                sorted_beams = sorted(new_beams, key=lambda x: x[1], reverse=True)
+                beams = sorted_beams[:beam_size]
+
+                # Check if any of the beams generated the end of sequence token
+                eos_beam = next((beam for beam in beams if beam[0][0, -1] == tokenizer.eos_token_id), None)
+                if eos_beam:
+                    decoder_input_ids, _ = eos_beam
+                    break
+
+            # Decode the output sequence
+            output_text = tokenizer.decode(decoder_input_ids[0], skip_special_tokens=True)
+
+            # Calculate the total output probability for the predicted beam
+            total_output_prob = beams[0][1]
+
+            return output_text, total_output_prob
 
     # Load pre-trained model and tokenizer
 
@@ -186,9 +243,14 @@ def main(args):
                 #print(input_part,"\t", ground_truth, "\t", output_text)
                 
                 # Generate completion and probabilities
-                output_text, output_probs = generate_completion_with_top_p(
-                    model, tokenizer, input_part
-                )
+                if args.inference_method is "beam":
+                    output_text, output_probs = generate_completion_with_beam_search(
+                        model, tokenizer, input_part
+                    )
+                elif args.inference_method is "top_p":
+                    output_text, output_probs = generate_completion_with_top_p(
+                        model, tokenizer, input_part
+                    )
 
                 # Calculate the average probability of the generated output
                 avg_prob = sum(output_probs) / len(output_probs) if output_probs else 0
